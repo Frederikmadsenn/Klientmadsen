29479c10cb3c88160e80ea9e3c2ec80c819abce6
Frederik Madsen <Madsen@Larss-MacBook-Pro-2.local>
2017-11-22 19:35:27 +0100

    login driller

--- fbm-client/js/Login.js
+++ fbm-client/js/Login.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
 
-    SDK.User.loadNav();
+    //SDK.User.loadNav();
 
 $("#login-button").click(() => {
 
-    const Username = $("#inputUsername").val();
+    const username = $("#inputUsername").val();
 const password = $("#inputPassword").val();
 
-SDK.User.login(email, password, (err, data) => {
+SDK.User.login(username, password, (err, data) => {
     if (err && err.xhr.status === 401) {
     $(".form-group").addClass("has-error");
 }
