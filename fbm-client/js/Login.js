009ed33f26e6fa8308f68e051eb6a986391692f4
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-11-26 10:13:33 +0100

    fix ignore

--- fbm-client/js/Login.js
+++ fbm-client/js/Login.js
@@ -1,23 +1,23 @@
 $(document).ready(() => {
 
-    //SDK.User.loadNav();
+  //SDK.User.loadNav();
 
-$("#login-button").click(() => {
+  $("#login-button").click(() => {
 
     const username = $("#inputUsername").val();
-const password = $("#inputPassword").val();
+    const password = $("#inputPassword").val();
 
-SDK.User.login(username, password, (err, data) => {
-    if (err && err.xhr.status === 401) {
-    $(".form-group").addClass("has-error");
-}
-else if (err){
-    console.log("stuff happened")
-} else {
-    window.location.href = "my-page.html";
-}
-});
+    SDK.User.login(username, password, (err, data) => {
+      if (err && err.xhr.status === 401) {
+        $(".form-group").addClass("has-error");
+      }
+      else if (err) {
+        console.log("stuff happened")
+      } else {
+        window.location.href = "my-page.html";
+      }
+    });
 
-});
+  });
 
 });
